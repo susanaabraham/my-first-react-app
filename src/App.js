d5c0a9e1dcd65a504bc5c01c5f38f7efd5094633
susanaabraham <susana_arns@hotmail.com>
2021-11-30 23:26:04 +0100

    pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title content="Some Simple Title" />
+        <Pokemon />
+      </main>
       <header className="App-header">
         <h1>Welcome to Coddaisseur</h1>
       </header>
